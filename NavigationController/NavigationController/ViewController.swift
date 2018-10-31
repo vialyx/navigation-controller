1b16b7725175585f6b294d4bb45d417100b0f9e2
Maxim Vialyx <maksimvialyx@gmail.com>
2018-10-31 22:52:14 +0300

    - initial commit

---
+++ NavigationController/NavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Maksim Vialykh on 31/10/2018.
+//  Copyright © 2018 Vialyx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
